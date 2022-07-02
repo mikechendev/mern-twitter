bee7931ffa067bcd28b887b89e1fb857f1f6e6b1
Michael Chen <michael.chen.1120@gmail.com>
2022-07-02 18:40:36 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const db = require('./config/keys').mongoURI;
+const users = require('./routes/api/users');
+const tweets = require('./routes/api/tweets');
+const bodyParser = require('body-parser');
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.log(err));
+
+app.get('/', (req, res) => res.send('uh hello?'));
+app.use('/api/users', users);
+app.use('/api/tweets', tweets);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
