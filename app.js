c0635b9f86297bcc1f687fb603e2dd0c1d3d7a0d
Michael Chen <michael.chen.1120@gmail.com>
2022-07-03 00:35:49 -0400

    fix user post

--- app.js
+++ app.js
@@ -5,16 +5,20 @@ const db = require('./config/keys').mongoURI;
 const users = require('./routes/api/users');
 const tweets = require('./routes/api/tweets');
 const bodyParser = require('body-parser');
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log('MongoDB Connected'))
   .catch((err) => console.log(err));
 
-app.get('/', (req, res) => res.send('uh hello?'));
-app.use('/api/users', users);
-app.use('/api/tweets', tweets);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('uh hello?');
+});
+app.use('/api/users', users);
+app.use('/api/tweets', tweets);
 
 const port = process.env.PORT || 5000;
 
